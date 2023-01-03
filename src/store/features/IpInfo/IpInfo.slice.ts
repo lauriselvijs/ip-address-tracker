388872bb8602485b7d26446842fe2507581b8f19
Lauris <lauriselvijsm@gmail.com>
2023-01-03 12:53:34 +0200

    updated config

--- src/store/features/IpInfo/IpInfo.slice.ts
+++ src/store/features/IpInfo/IpInfo.slice.ts
@@ -1,23 +1,18 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-import { IpInfo, IpInfoError } from "../../../types/IpInfo";
+import { IpInfo } from "../../../types/IpInfo";
 
-import {
-  IP_API_BASE_URL,
-  IP_API_GET_IP_INFO,
-  IP_INFO_API_REDUCER_PATH,
-  SUCCESS,
-} from "./IpInfo.config";
+import { BASE_URL, GET_IP_INFO, REDUCER_PATH, SUCCESS } from "./IpInfo.config";
 
 export const IpInfoApi = createApi({
-  reducerPath: IP_INFO_API_REDUCER_PATH,
+  reducerPath: REDUCER_PATH,
   baseQuery: fetchBaseQuery({
-    baseUrl: IP_API_BASE_URL,
+    baseUrl: BASE_URL,
   }),
   endpoints: (builder) => ({
     getIpInfo: builder.query<IpInfo, string>({
       query: (ip) => ({
-        url: `${IP_API_GET_IP_INFO}${ip}`,
+        url: `${GET_IP_INFO}${ip}`,
         validateStatus: (response, result) =>
           response.status === 200 && result.status === SUCCESS,
       }),
