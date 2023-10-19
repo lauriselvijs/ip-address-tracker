63ceca55a84a141b47be47908058e24de83c12bf
Lauris <85683069+lauriselvijs@users.noreply.github.com>
2023-10-19 11:51:48 +0300

    Update Font.style.ts

--- src/style/base/Font.style.ts
+++ src/style/base/Font.style.ts
@@ -9,7 +9,7 @@ export const font = `
 
 @font-face {
     font-family: Rubik;
-    src: url('Rubik-Medium.woff2') format('woff2');
+    src: url('../font/Rubik/Rubik-Medium.woff2') format('woff2');
     font-weight: 500;
     font-style: normal;
     font-display: swap;
@@ -17,7 +17,7 @@ export const font = `
 
 @font-face {
     font-family: Rubik;
-    src: url('Rubik-Regular.woff2') format('woff2');
+    src: url('../font/Rubik/Rubik-Regular.woff2') format('woff2');
     font-weight: 400;
     font-style: normal;
     font-display: swap;
