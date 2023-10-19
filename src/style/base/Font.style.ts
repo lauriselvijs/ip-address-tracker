7e0cdf7d16a2fd4be9927fc01c0023a3b3353c66
Lauris <85683069+lauriselvijs@users.noreply.github.com>
2023-10-19 11:50:36 +0300

    Update Font.style.ts

--- src/style/base/Font.style.ts
+++ src/style/base/Font.style.ts
@@ -1,7 +1,7 @@
 export const font = `
 @font-face {
     font-family: Rubik;
-    src: url('Rubik-Bold.woff2') format('woff2');
+    src: url('../font/Rubik/Rubik-Bold.woff2') format('woff2');
     font-weight: 700;
     font-style: normal;
     font-display: swap;
